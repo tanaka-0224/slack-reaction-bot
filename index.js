0cd8a5ff7b56c54f1d8e881dd00fec56e6ec14f3
tanaka-0224 <r.iku0224@icloud.com>
2025-05-12 18:46:38 +0900

    test1

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.message(async ({ message, client }) => {
+  await client.reactions.add({
+    name: "snake",
+    channel: message.channel,
+    timestamp: message.ts,
+  });
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("⚡️ Bot is running");
+})();
